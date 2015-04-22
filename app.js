7d32a0f792881c5c63cd0da4560964230abf2392
Alex Booker <alexbooker@fastmail.im>
2015-04-22 19:46:06 +0100

    installed bootstrap

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var path    = require('path');
 
 var app = express();
+
 app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.render('index');
