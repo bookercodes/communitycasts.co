e9626442229197b7c3036d39a11c0b8ef51e451c
Alex Booker <alexbooker@fastmail.im>
2015-04-22 20:17:09 +0100

    added submit view

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.technologies = [ 
+    'Java', 
+    'Javascript', 
+    'Node', 
+    'Mongo',
+    'C#'
+  ];
+  next();
+});
+
 app.get('/', function (req, res) {
-  res.render('index', { 
-    technologies: [ 
-      'Java', 
-      'Javascript', 
-      'Node', 
-      'Mongo',
-      'C#'] 
-  });
+  res.render('index');
+});
+
+app.get('/submit', function(req, res) {
+  res.render('submit');
 });
 
 app.listen(3000);
