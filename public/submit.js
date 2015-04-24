99e55f845a67cac09ea2c497da9235cc85f0f793
Alex Booker <alexbooker@fastmail.im>
2015-04-24 19:17:10 +0100

    extract YouTube video ID from video URL

--- public/submit.js
+++ public/submit.js
@@ -1,13 +1,22 @@
 $(function() {
 
+  // http://stackoverflow.com/a/9102270/4804328
+  function extractId(url) {
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if (match && match[2].length == 11) {
+      return match[2];
+    }
+  }
+
   function buildApiUrl(parts, id, key) {
     var base = "https://www.googleapis.com/youtube/v3/videos";
     return base + "?part=" + parts + "&id=" + id + "&key=" + key;
   }
-  
+
   $("#url").change(function() {
     var videoUrl = $(this).val();
-    var id = videoUrl;
+    var id = extractId(videoUrl);
     var parts = "snippet,contentDetails";
     var key = "AIzaSyCKQFYlDRi5BTd1A-9rhFjF8Jb_Hlfnquk";
     var apiUrl = buildApiUrl(parts, id, key);
