87ff1690bb9569d6aecde16491cd7961bdc78a9d
Alex Booker <alexbooker@fastmail.im>
2015-04-24 19:32:33 +0100

    added schema

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE videoHub;
+CREATE DATABASE videoHub;
+USE videoHub;
+
+CREATE TABLE technologies (
+  technologyName VARCHAR(50) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE videos (
+  videoId     INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title       VARCHAR(300) NOT NULL,
+  description VARCHAR(300) NOT NULL,
+  url         VARCHAR(300) NOT NULL,
+  channelName VARCHAR(300) NOT NULL/*,
+  referrals   INT          NOT NULL,
+  slug        VARCHAR(300) NOT NULL*/
+);
+
+CREATE TABLE technology_video_map (
+  videoId        INT,
+  technologyName VARCHAR(50),
+
+  PRIMARY KEY (videoId, technologyName),
+  FOREIGN KEY (videoId) REFERENCES videos(videoId),
+  FOREIGN KEY (technologyName) REFERENCES technologies(technologyName)
+);
