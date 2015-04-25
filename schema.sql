8788c5ed20de737061b889600448f4778f390cc2
Alex Booker <alexbooker@fastmail.im>
2015-04-25 23:58:08 +0100

    updated schema

--- schema.sql
+++ schema.sql
@@ -3,33 +3,39 @@ CREATE DATABASE videoHub;
 USE videoHub;
 
 CREATE TABLE technologies (
-  technologyName VARCHAR(50) NOT NULL PRIMARY KEY
+  technologyName VARCHAR(25) NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE channels (
+  channelId INT AUTO_INCREMENT PRIMARY KEY,
+  url       VARCHAR(24)  NOT NULL,
+  name      VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE videos (
-  videoId        INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title          VARCHAR(300) NOT NULL,
-  description    VARCHAR(300) NOT NULL,
-  url            VARCHAR(300) NOT NULL,
-  channelName    VARCHAR(300) NOT NULL,
-  referrals      INT          NOT NULL,
-  approved       BIT          NOT NULL DEFAULT 1,
-  submissionDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  videoId           INT AUTO_INCREMENT PRIMARY KEY,
+  channelId         INT REFERENCES channels(channelId),
+  url               VARCHAR(11)  NOT NULL,
+  title             VARCHAR(100) NOT NULL,
+  description       TEXT         NOT NULL,
+  thumbnailUrl      VARCHAR(48)  NOT NULL,
+  durationInSeconds INT          NOT NULL,
+  hd                BIT          NOT NULL,
+  approved          BIT          NOT NULL DEFAULT 1,
+  submissionDate    TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  referrals         INT          NOT NULL
+);
+
+CREATE TABLE technologies_video_map (
+  videoId        INT         REFERENCES videos(videoId),
+  technologyName VARCHAR(25) REFERENCES technologies(technologyName),
+
+  PRIMARY KEY (videoId, technologyName) 
 );
 
 CREATE TABLE referrals (
-  videoId   INT         NOT NULL,
+  videoId   INT         NOT NULL REFERENCES videos(videoId),
   refereeIp VARCHAR(20) NOT NULL,
 
-  PRIMARY KEY (videoId, refereeIp),
-  FOREIGN KEY (videoId) REFERENCES videos(videoId)
-);
-
-CREATE TABLE technology_video_map (
-  videoId        INT,
-  technologyName VARCHAR(50),
-
-  PRIMARY KEY (videoId, technologyName),
-  FOREIGN KEY (videoId) REFERENCES videos(videoId),
-  FOREIGN KEY (technologyName) REFERENCES technologies(technologyName)
+  PRIMARY KEY (videoId, refereeIp)
 );
