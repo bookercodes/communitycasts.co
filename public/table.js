4eafd8fd0498487752b16e87950b0b919094d9ed
Alex Booker <alexbooker@fastmail.im>
2015-04-27 21:34:04 +0100

    added formatters for grid

---
+++ public/table.js
@@ -0,0 +1,30 @@
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  );
+};
+
+function titleFormatter(value, row) {
+  return "<a href='/videos/{videoId}'>{title}</a>".supplant(row);
+}
+
+function channelNameFormatter(value, row) {
+  return "<i class='fa fa-user'/>{channelName}".supplant(row);
+}
+
+function durationFormatter(value, row) {
+  return "<i class='fa fa-clock-o'/>{duration}".supplant(row);
+}
+
+function technologiesFormatter(value, row) {
+  var html = "<a href='technologies/{technology1}'>{technology1}</a>";
+  if (row.technologies[1] != null)
+    html += " <a href='technologies/{technology2}'>{technology2}</a>";
+  return html.supplant({
+    technology1: row.technologies[0],
+    technology2: row.technologies[1],
+  });
+}
