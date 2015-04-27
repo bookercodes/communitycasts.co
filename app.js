763cb3f70edbb5a0b0b0620cf069c0557883fa51
Alex Booker <alexbooker@fastmail.im>
2015-04-27 19:59:09 +0100

    added server-side support for pagination

--- app.js
+++ app.js
@@ -244,4 +244,60 @@ app.get('/test', function(req, res) {
   res.render('test');
 });
 
+app.get('/data/', function(req,res) {
+
+  var model = {};
+  connection.query('select count(*) as total from videos; ', function(err, result) {
+    model.total = result[0].total;
+
+    var query = 
+      'select \
+         v.videoId, \
+         v.title, \
+         c.channelId, \
+         c.channelName, \
+         v.durationInSeconds, \
+         v.submissionDate, \
+         GROUP_CONCAT(m.technologyName) as technologies \
+      from videos v \
+      join channels c \
+        on c.channelId = v.channelId \
+      join technology_video_map m \
+        on m.videoId = v.videoId \
+      where v.approved = 1 \
+      group by v.videoId \
+      limit ' + req.query.offset + ', ' + req.query.limit
+
+    connection.query(query, function (err, result) {
+      result.forEach(function(record) {
+        record.technologies = record.technologies.split(',');
+        record.duration = moment.duration(record.durationInSeconds, 'seconds').humanize();
+        delete record.durationInSeconds;
+      });
+      model.rows = result;
+      res.send(model);
+    });
+
+  });
+
+  // var model = {};
+  // model.total = 10;
+  // model.rows = [
+  //   {  
+  //     "id":31,
+  //     "name":"test31",
+  //     "price":"$31"
+  //   },
+  //   {  
+  //     "id":32,
+  //     "name":"test32",
+  //     "price":"$32"
+  //   }
+  // ];
+
+  // res.send(model);
+
+
+});
+
 app.listen(53111);
