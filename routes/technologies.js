5b26ce2e078863146ae38217c0777a3ef4395559
Alex Booker <alexbooker@fastmail.im>
2015-04-29 19:18:07 +0100

    added pagination to technologies pages

--- routes/technologies.js
+++ routes/technologies.js
@@ -48,8 +48,21 @@ router.get('/other', function(req, res) {
 });
 
 router.get('/:technology', function (req, res) {
-  var locals = {};
-  locals.technologyName = req.params.technology;
+  res.render('technology', {technologyName:req.params.technology});
+});
+
+router.get('/api/:technology',function(req,res) { 
+  var body = {};
+  var query = 
+  'select count(*) as total \
+   from technologies t \
+   join technology_video_map m \
+     on m.technologyName = t.technologyName \
+   where t.technologyName = ' + connection.escape(req.params.technology) + '\
+   group by t.technologyName';
+
+  connection.query(query, function(err, result) {
+    body.total = result[0].total;
     var query = 
     'select \
        v.videoId, \
@@ -68,17 +81,54 @@ router.get('/:technology', function (req, res) {
       select videoId \
       from technology_video_map m \
       where m.technologyName = ' + connection.escape(req.params.technology) + ') \
-      group by v.videoId ';
+      group by v.videoId';
+    if (req.query.kind === 'popular')
+      query += ' order by v.referrals desc';
+    else
+      query += ' order by v.submissionDate desc';
+    query += ' limit ' + req.query.offset + ', ' + req.query.limit;
 
-  connection.query(query + 'order by v.referrals desc', function(err, popularVideos) {
-    common.convertRecordsToLocals(popularVideos);
-    locals.popularVideos = popularVideos;
-    connection.query(query + 'order by v.submissionDate desc', function(err, newVideos) {
-      common.convertRecordsToLocals(newVideos);
-      locals.newVideos = newVideos;
-      res.render('technology', locals);
+    connection.query(query, function(err, popularVideos) {
+      if (err) { res.send(err); return;}
+      common.convertRecordsToLocals(popularVideos);
+      body.rows = popularVideos;
+      res.send(body);
     });
   });
 });
+
+// router.get('api/:technology', function (req, res) {
+//   var locals = {};
+//   locals.technologyName = req.params.technology;
+//     var query = 
+//     'select \
+//        v.videoId, \
+//        v.title, \
+//        c.channelId, \
+//        c.channelName, \
+//        v.durationInSeconds, \
+//        v.submissionDate, \
+//        GROUP_CONCAT(m.technologyName) as technologies \
+//     from videos v \
+//     join channels c \
+//       on c.channelId = v.channelId \
+//     join technology_video_map m \
+//       on m.videoId = v.videoId \
+//     where v.approved = 1 and v.videoId in ( \
+//       select videoId \
+//       from technology_video_map m \
+//       where m.technologyName = ' + connection.escape(req.params.technology) + ') \
+//       group by v.videoId ';
+
+//   connection.query(query + 'order by v.referrals desc', function(err, popularVideos) {
+//     common.convertRecordsToLocals(popularVideos);
+//     locals.popularVideos = popularVideos;
+//     connection.query(query + 'order by v.submissionDate desc', function(err, newVideos) {
+//       common.convertRecordsToLocals(newVideos);
+//       locals.newVideos = newVideos;
+//       res.render('technology', locals);
+//     });
+//   });
+// });
 
 module.exports = router;
