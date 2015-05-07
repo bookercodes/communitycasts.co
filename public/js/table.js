66fb2a9e4e25881553f8db580ccb8ecbf9930d36
Alex Booker <alexbooker@fastmail.im>
2015-05-07 20:58:25 +0100

    added tooltips to table definitions

--- public/js/table.js
+++ public/js/table.js
@@ -8,21 +8,21 @@ String.prototype.supplant = function (o) {
 };
 
 function titleFormatter(value, row) {
-  return "<a target=\"_blank\" href='/screencasts/{screencastId}'>{title}</a>".supplant(row);
+  return "<span title='Screencast title'><a target=\"_blank\" href='/screencasts/{screencastId}'>{title}</a></span>".supplant(row);
 }
 
 function channelNameFormatter(value, row) {
-  return "<i class='fa fa-user'/>{channelName}".supplant(row);
+  return "<span title='Channel name'><i class='fa fa-user'/>{channelName}</span>".supplant(row);
 }
 
 function durationFormatter(value, row) {
-  return "<i class='fa fa-clock-o'/>{duration}".supplant(row);
+  return "<span title='Screencast duration'><i class='fa fa-clock-o'/>{duration}</span>".supplant(row);
 }
 
 function technologiesFormatter(value, row) {
-  var html = "<i class='fa fa-folder-o'/> <a href='/technologies/"+ encodeURIComponent(row.technologies[0]) + "'>{technology1}</a>";
+  var html = "<i class='fa fa-folder-o'/> <a href='/technologies/"+ encodeURIComponent(row.technologies[0]) + "' title='Tag 1'>{technology1}</a>";
   if (row.technologies[1] != null)
-    html += " <a href='technologies/" + encodeURIComponent(row.technologies[0]) +"'>{technology2}</a>";
+    html += " <a href='technologies/" + encodeURIComponent(row.technologies[0]) +"'  title='Tag 2'>{technology2}</a>";
   return html.supplant({
     technology1: row.technologies[0],
     technology2: row.technologies[1],
