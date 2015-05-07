62574b8da908a94007e9c81649ed68ef741a93cb
Alex Booker <alexbooker@fastmail.im>
2015-05-07 19:35:15 +0100

    submit route now uses promises

--- routes/submit.js
+++ routes/submit.js
@@ -11,72 +11,48 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function (req, res) {
+
   req.checkBody('url', 'YouTube Url is missing.').notEmpty();
-  req.checkBody('url', 'Url must be a YouTube url.')
-    .matches(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/);
+  req.checkBody('url', 'Url must be a YouTube url.').matches(/^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/);
   req.checkBody('technologies', 'Please enter at least one technology.').notEmpty();
   var errors = req.validationErrors();
   if (errors) {
     res.render('submit', { errors: errors });
     return;
   }
-  var videoId = youtube.parseIdFromUrl(req.body.url);
-  var query = 'SELECT * FROM screencasts WHERE screencastId = ' + connection.escape(videoId);
-  connection.query(query, function (err, result) {
-    // if the query results in one or more records, then a video with this
-    // id already exists and we must reject it.
-    var alreadyExists = result.length >= 1;
-    if (alreadyExists) {
-      res.render('submit', { errors: [{ msg:'This video has already been submitted.' }] })
+
+  var screencastId = youtube.parseIdFromUrl(req.body.url);
+  var tags = req.body.technologies.split(',');
+
+  var query = 
+    'SELECT screencastId \
+     FROM screencasts \
+     WHERE screencastId = ?';
+  connection.queryAsync(query, screencastId).spread(function (screencasts) {
+    if (screencasts.length === 1) {
+      res.render('submit', { errors: [{ msg: 'This video has already been submitted.' }] })
       return;
     }
-    youtube.get(videoId).then(function(video) {
-      if (video == null) {
-        res.render('submit', { errors: [{ msg:'This video could not be found.' }] })
+    youtube.get(screencastId).then(function(screencast) {
+      if (screencast === null) {
+        res.render('submit', { errors: [{ msg:'This screencast could not be found.' }] })
         return;
       }
-      connection.query('INSERT IGNORE INTO channels SET ?', video.channel, function(err, result) {
-                  if (err) {
-            console.log(err);
-          }
+      connection.queryAsync('INSERT IGNORE INTO channels SET ?', screencast.channel).then(function(result) {
         var record = {
-          screencastId: videoId,
-          channelId: video.channel.channelId,
-          title: video.title,
-          durationInSeconds: moment.duration(video.duration).asSeconds(),
+          screencastId: screencastId,
+          channelId: screencast.channel.channelId,
+          title: screencast.title,
+          durationInSeconds: moment.duration(screencast.duration).asSeconds(),
         }
-        connection.query('INSERT INTO screencasts SET ?', record, function(err, result) {
-          if (err) {
-            console.log(err);
-          }
-          var technologies = req.body.technologies.split(',');
-          var query = 'INSERT IGNORE INTO tags VALUES ';
-          technologies.forEach(function(technology) {
-            query += '(' + connection.escape(technology) + '),';
-          });
-          query = query.substr(0, query.length - 1);
-
-          connection.query(query, function(err, result) {
-                      if (err) {
-            console.log(err);
-          }
-            technologies.forEach(function(technology, index, array) {
-              var record = { 
-                screencastId: videoId, 
-                tagName: technology 
-              }
-              connection.query('INSERT INTO screencastTags SET ?', record, function(err, result) {
-                          if (err) {
-            console.log(err);
-          }
-                // if this is the final iteration, redirect the user.
-                if (index === array.length - 1) {
-                  res.redirect('/');
-                }
-              });
-            }); 
-          })
-        });
+        return connection.query('INSERT INTO screencasts SET ?', record);
+      }).then(function(result) {
+          var values = tags.map(function(tag) { return [tag]; });
+          return connection.queryAsync('INSERT IGNORE INTO tags VALUES ?', [values])
+      }).then(function(result) {
+        var values = tags.map(function(tag) { return [screencast.screencastId, tag]; });
+        connection.queryAsync('INSERT INTO screencastTags VALUES ?', [values])
+        res.redirect('/');
       });
     });
   });
