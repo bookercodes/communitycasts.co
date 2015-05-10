90db65d11767b8290b663fb867cc22a478953852
Alex Booker <alexbooker@fastmail.im>
2015-05-10 18:47:30 +0100

    added admin script

---
+++ admin.js
@@ -0,0 +1,48 @@
+var promise = require('bluebird');
+var prompt = require('prompt');
+var mysql   = require('mysql');
+var async = require('async');
+
+promise.promisifyAll(prompt);
+
+promise.promisifyAll(require('mysql/lib/Connection').prototype);
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'screencastHub'
+});
+connection.connect();
+
+prompt.start();
+
+var property = {
+  name: 'approve',
+  message: 'approve this screencast?',
+  validator: /y[es]*|n[o]?/,
+  warning: 'Must respond yes or no',
+  default: 'yes'
+};
+
+
+function process(screencastId, approve) {
+  if (approve) {
+    connection.queryAsync('UPDATE screencasts SET approved = 1 WHERE screencastId = ?', screencastId);
+  }
+}
+
+connection.queryAsync('SELECT * FROM screencasts WHERE approved = 0').spread(function(screencasts) {
+  var current = promise.resolve();
+  promise.map(screencasts, function(screencast) {
+    current = current.then(function() {
+      console.log('Title: "' + screencast.title + '".');
+      return prompt.getAsync(property);
+    }).then(function(result) {
+      process(
+        screencast.screencastId, 
+        result.approve === 'yes')
+    });
+    return current;
+  });
+});
