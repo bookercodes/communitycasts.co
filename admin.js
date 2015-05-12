52c9284400f7bf21c3149a10f548baf0acae7e73
Alex Booker <alexbooker@fastmail.im>
2015-05-12 23:51:35 +0100

    developed admin script

--- admin.js
+++ admin.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
+var promise  = require('bluebird');
 var readline = require('readline');
-var promise = require('bluebird');
-var prompt = require('prompt');
-var mysql   = require('mysql');
-
-var terminal = readline.createInterface(process.stdin, process.stdout);
-
-// promise.promisifyAll(prompt);
+var mysql    = require('mysql');
+var prompt   = require('prompt');
 
 promise.promisifyAll(require('mysql/lib/Connection').prototype);
+promise.promisifyAll(prompt);
 
+var terminal = readline.createInterface(process.stdin, process.stdout);
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -20,61 +18,74 @@ var connection = mysql.createConnection({
 });
 connection.connect();
 
-terminal.question('Source tag: ', function(sourceTag) {
-  terminal.question('Target tag: ', function(targetTag) {
-    var param = { 
-      sourceTagName: sourceTag, 
-      targetTagName: targetTag 
-    };
-    connection.queryAsync('INSERT INTO tagSynonyms SET ?', param).then(function() {
-      var query = 
-        'UPDATE screencastTags \
-         JOIN tagSynonyms \
-           ON screencastTags.tagName = tagSynonyms.sourceTagName \
-         SET \
-           tagName = tagSynonyms.targetTagName; \
-         DELETE \
-         FROM tags \
-         WHERE tagName NOT IN ( \
-           SELECT tagName \
-           FROM screencastTags \
-           WHERE screencastTags.tagName = tags.tagName)';
-      connection.queryAsync(query);
-    });
-  });
+console.log('Hello. Here are your options:')
+console.log(' 1. Approve pending screencasts interactively \n 2. Define tag synonym')
+
+terminal.question('Option: ', function(option) {
+  if (option === '1') {
+    approve();
+  } else if (option === '2') {
+    tagSynonym();
+  } else {
+    console.log('Invalid option.');
+    process.exit(1);
+  }
+  terminal.close();
 });
 
+function tagSynonym() {
+  terminal.question('Source tag: ', function(sourceTag) {
+    terminal.question('Target tag: ', function(targetTag) {
+      var param = { 
+        sourceTagName: sourceTag, 
+        targetTagName: targetTag 
+      };
+      connection.queryAsync('INSERT INTO tagSynonyms SET ?', param).then(function() {
+        var query = 
+          'UPDATE screencastTags \
+           JOIN tagSynonyms \
+             ON screencastTags.tagName = tagSynonyms.sourceTagName \
+           SET \
+             tagName = tagSynonyms.targetTagName; \
+           DELETE \
+           FROM tags \
+           WHERE tagName NOT IN ( \
+             SELECT tagName \
+             FROM screencastTags \
+             WHERE screencastTags.tagName = tags.tagName)';
+        connection.queryAsync(query).then(function() {
+          console.log('Added synonym and applied mapping.');
+          process.exit(0);
+        })
+      });
+    });
+  });
+}
 
-// prompt.start();
+var property = {
+  name: 'approve',
+  message: 'approve this screencast?',
+  validator: /y[es]*|n[o]?/,
+  warning: 'Must respond yes or no',
+  default: 'yes'
+};
 
-// var property = {
-//   name: 'approve',
-//   message: 'approve this screencast?',
-//   validator: /y[es]*|n[o]?/,
-//   warning: 'Must respond yes or no',
-//   default: 'yes'
-// };
-
-
-// function process(screencastId, approve) {
-//   if (approve) {
-//     connection.queryAsync('UPDATE screencasts SET status = \'approved\' WHERE screencastId = ?', screencastId);
-//   } else {
-//     connection.queryAsync('UPDATE screencasts SET status = \'denied\' WHERE screencastId = ?', screencastId);
-//   }
-// }
-
-// connection.queryAsync('SELECT * FROM screencasts WHERE status = \'pending\'').spread(function(screencasts) {
-//   var current = promise.resolve();
-//   promise.map(screencasts, function(screencast) {
-//     current = current.then(function() {
-//       console.log('Title: "' + screencast.title + '".');
-//       return prompt.getAsync(property);
-//     }).then(function(result) {
-//       process(
-//         screencast.screencastId, 
-//         result.approve === 'yes')
-//     });
-//     return current;
-//   });
-// });
+function approve() {
+  prompt.start();
+  connection.queryAsync('SELECT * FROM screencasts WHERE status = \'pending\'').spread(function(screencasts) {
+  var current = promise.resolve();
+  promise.map(screencasts, function(screencast) {
+    current = current.then(function() {
+      console.log('Title: "' + screencast.title + '".');
+      return prompt.getAsync(property);
+    }).then(function(result) {
+      if (result.approve === 'yes') {
+        connection.queryAsync('UPDATE screencasts SET status = \'approved\' WHERE screencastId = ?', screencast.screencastId)
+      } else {
+        connection.queryAsync('UPDATE screencasts SET status = \'denied\' WHERE screencastId = ?', screencast.screencastId);
+      }
+    });
+    return current;
+  });
+});
+}
