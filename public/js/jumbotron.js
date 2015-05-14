6441c3bdd97ae509ed1faa3bb796ee5bd3adbfa4
Alex Booker <alexbooker@fastmail.im>
2015-05-14 15:29:02 +0100

    added close button to the jumbotron

---
+++ public/js/jumbotron.js
@@ -0,0 +1,21 @@
+$(function() {
+  
+  var x = getCookie('jumbotron-visible');
+  if (x != "false") {
+    $(".jumbotron").show();
+  }
+  
+  $("button.close").click(function() {
+    $(".jumbotron").hide();
+    setCookie('jumbotron-visible', 'false');
+  });
+
+  function setCookie(c_name, value) {
+    Cookies.set(c_name, value, { expires: 365 });
+  }
+
+  function getCookie(c_name) {
+    return Cookies.get(c_name);
+  }  
+});
+
