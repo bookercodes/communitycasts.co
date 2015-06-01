0cd63010b7b360c6691dae31871b796de012e361
Alex Booker <alexbooker@fastmail.im>
2015-06-01 12:27:50 +0100

    added GA

--- source/flightplan.js
+++ source/flightplan.js
@@ -30,6 +30,4 @@ plan.remote(function(remote) {
 
   remote.log('Reload application');
   remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
-  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
-  remote.exec('forever start ~/'+appName+'/'+startFile);
 });
