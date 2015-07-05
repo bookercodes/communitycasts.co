1f6f9884f8e4f7dd6256f6c53ac6182e84687fd6
Alex Booker <alexbooker@fastmail.im>
2015-07-05 12:47:14 +0100

    refactored youtube.js to use module pattern

--- source/youtube.js
+++ source/youtube.js
@@ -2,39 +2,42 @@ var Promise = require('bluebird');
 var moment  = require('moment');
 var request = Promise.promisify(require('request'));
 
-function Youtube() {
-}
-
-Youtube.prototype.authenticate = function(key) {
-  this.key = key;
-};
-
-Youtube.prototype.get = function (id) {
-  var url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=' + id + '&key=' + this.key;
-  return request(url).spread(function(request, body) {
-    var parsed = JSON.parse(body);
-    if (parsed.items.length === 0) {
-      // no video with id 'id' found
-      return null;      
-    }
-    var screencast = parsed.items[0];
-    return {
-      screencastId: id,
-      title: screencast.snippet.title,
-      durationInSeconds: moment.duration(screencast.contentDetails.duration).asSeconds(),
-      channel: {
-        channelName: screencast.snippet.channelTitle,
-        channelId: screencast.snippet.channelId,  
+var youtube = (function() {
+  var authenticate = function(key) {
+    this.key = key;
+  };
+  var get = function (id) {
+    var url = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=' + id + '&key=' + this.key;
+    return request(url).spread(function(request, body) {
+      var parsed = JSON.parse(body);
+      if (parsed.items.length === 0) {
+        // no video with id 'id' found
+        return null;
       }
-    };
-  });
-};
+      var screencast = parsed.items[0];
+      return {
+        screencastId: id,
+        title: screencast.snippet.title,
+        durationInSeconds: moment.duration(screencast.contentDetails.duration).asSeconds(),
+        channel: {
+          channelName: screencast.snippet.channelTitle,
+          channelId: screencast.snippet.channelId,
+        }
+      };
+    });
+  };
+  var parseIdFromUrl = function (url) {
+    var pattern = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(pattern);
+    if (match && match[2].length == 11) {
+      return match[2];
+    }
+  };
+  return {
+    authenticate: authenticate,
+    get: get,
+    parseIdFromUrl: parseIdFromUrl
+  };
+})();
 
-Youtube.prototype.parseIdFromUrl = function (url) {
-  var pattern = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-  var match = url.match(pattern);
-  if (match && match[2].length == 11) {
-    return match[2];
-  }
-}
-module.exports = new Youtube();
+module.exports = youtube;
