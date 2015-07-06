719a8bb2af23e277c40def9367fbfb734086dae9
Alex Booker <alexbooker@fastmail.im>
2015-07-06 21:25:07 +0100

    added simple route handler

--- source/server/server.js
+++ source/server/server.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'communityCasts'
+});
+connection.connect();
 
 var app = express();
 
-app.get('*', function (req, res) {
-  res.json({ message:'Hello, world' });
+app.get('/screencasts', function (req, res) {
+  var query = 'SELECT * FROM screencasts';
+  connection.query(query, function (error, screencasts) {
+    res.json(screencasts);
+  });
 });
 
 app.listen(3000);
