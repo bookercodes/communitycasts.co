f5c1c4bcb11e394d3d773f6ae9786fde020d4087
Alex Booker <alexbooker@fastmail.im>
2015-07-08 21:54:13 +0100

    added pagination to the server

--- source/client/app/app.js
+++ source/client/app/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('communityCasts', []);
+var app = angular.module('communityCasts', ['infinite-scroll']);
--- source/client/app/controllers/homeController.js
+++ source/client/app/controllers/homeController.js
@@ -1,5 +1,8 @@
 app.controller('homeController', function ($scope, $http) {
 
+  $scope.fetchNextPage = function() {
+    console.log("Shit is happening, brah.");
+  };
 
   $http.get('http://localhost:3000/screencasts/top/today').success(function (data) {
     $scope.screencasts = data;
