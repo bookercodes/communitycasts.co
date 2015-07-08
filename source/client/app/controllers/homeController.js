a7087f567bbd0ed9117e517d32722e162034510a
Alex Booker <alexbooker@fastmail.im>
2015-07-08 22:16:34 +0100

    added basic pagination to the client

--- source/client/app/controllers/homeController.js
+++ source/client/app/controllers/homeController.js
@@ -1,16 +1,13 @@
 app.controller('homeController', function ($scope, $http) {
-
-  $scope.fetchNextPage = function() {
-    console.log("Shit is happening, brah.");
-  };
-
-  $http.get('http://localhost:3000/screencasts/top/today').success(function (data) {
-    $scope.screencasts = data;
-  });
-
-  $scope.fetchScreencasts = function () {
-    $http.get('http://localhost:3000/screencasts/top/' + $scope.sortOption).success(function (data) {
-      $scope.screencasts = data;
+  $scope.page = 1;
+  $scope.screencasts = [];
+  $scope.sortOption = 'today';
+  $scope.fetchScreencasts = function() {
+    var base = 'http://localhost:3000/screencasts';
+    var url = base + '/top/' + $scope.sortOption + '?page=' + $scope.page;
+    $http.get(url).success(function (screencasts) {
+      $scope.screencasts = $scope.screencasts.concat(screencasts);
+      $scope.page += 1;
     });
   };
 });
