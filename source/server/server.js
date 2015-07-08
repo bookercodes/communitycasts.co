c466562c746f29b9b68fb82e7df35be849c2a4eb
Alex Booker <alexbooker@fastmail.im>
2015-07-08 19:30:51 +0100

    added screencasts route handler

--- source/server/server.js
+++ source/server/server.js
@@ -14,7 +14,25 @@ var app = express();
 app.use(cors());
 
 app.get('/screencasts', function (req, res) {
-  var query = 'SELECT * FROM screencasts';
+  var query = 'SELECT * FROM screencasts LIMIT 10';
+  connection.query(query, function (error, screencasts) {
+    res.json(screencasts);
+  });
+});
+
+app.get('/screencasts/top/:period', function (req, res) {
+  var query = 'SELECT * FROM screencasts s';
+  switch (req.params.period) {
+    case 'month':
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
+      break;
+    case 'week':
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
+      break;
+    default:
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
+      break;
+  }
   connection.query(query, function (error, screencasts) {
     res.json(screencasts);
   });
