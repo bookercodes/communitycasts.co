7376547856d0002c04bd9e466077d766690d4dad
Alex Booker <alexbooker@fastmail.im>
2015-07-08 17:53:47 +0100

    added not null attribute to columns who have default value

--- source/server/schema.sql
+++ source/server/schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE screencasts (
   screencastId        INT AUTO_INCREMENT,
   title               NVARCHAR(200) NOT NULL,
   durationInSeconds   INT           NOT NULL,
-  submissionDate      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  referralCount       INT       DEFAULT 0,
+  submissionDate      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  referralCount       INT           NOT NULL DEFAULT 0,
   PRIMARY KEY(screencastId)
 );
