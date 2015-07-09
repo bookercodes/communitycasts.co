f95470ef02be8432096cc9d21b8118b175b2602f
Alex Booker <alexbooker@fastmail.im>
2015-07-09 22:07:32 +0100

    setup client route infrastructure

--- source/client/app/app.js
+++ source/client/app/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('communityCasts', []);
+var app = angular.module('communityCasts', ['ui.router']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider.state('main', {
+    url: '/',
+    templateUrl: '/app/views/home.html',
+    controller: 'homeController'
+  });
+  $stateProvider.state('about', {
+    url: '/about',
+    templateUrl: '/app/views/about.html'
+  });
+});
