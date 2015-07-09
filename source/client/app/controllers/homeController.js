788c972761bd9a201a79eb563f2ae08471ffee13
Alex Booker <alexbooker@fastmail.im>
2015-07-09 18:03:40 +0100

    added load more button

--- source/client/app/controllers/homeController.js
+++ source/client/app/controllers/homeController.js
@@ -2,12 +2,17 @@ app.controller('homeController', function ($scope, $http) {
   $scope.page = 1;
   $scope.screencasts = [];
   $scope.sortOption = 'today';
-  $scope.fetchScreencasts = function() {
+  $scope.hasMore = true;
+  $scope.fetchScreencasts = function () {
     var base = 'http://localhost:3000/screencasts';
     var url = base + '/top/' + $scope.sortOption + '?page=' + $scope.page;
-    $http.get(url).success(function (screencasts) {
-      $scope.screencasts = $scope.screencasts.concat(screencasts);
+    $http.get(url).success(function (body) {
+      $scope.screencasts = $scope.screencasts.concat(body.screencasts);
       $scope.page += 1;
+      if (!body.hasMore) {
+        $scope.hasMore = false;
+      }
     });
   };
+  $scope.fetchScreencasts();
 });
--- source/server/server.js
+++ source/server/server.js
@@ -13,28 +13,36 @@ connection.connect();
 var app = express();
 app.use(cors());
 
-app.get('/screencasts', function (req, res) {
-  var query = 'SELECT * FROM screencasts LIMIT 10';
-  connection.query(query, function (error, screencasts) {
-    res.json(screencasts);
-  });
-});
-
 app.get('/screencasts/top/:period', function (req, res) {
-  var query = 'SELECT * FROM screencasts s';
-  switch (req.params.period) {
-    case 'month':
-      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
-      break;
-    case 'week':
-      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
-      break;
-    default:
-      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
-      break;
-  }
-  connection.query(query, function (error, screencasts) {
-    res.json(screencasts);
+
+  connection.query('SELECT COUNT(*) AS count FROM screencasts', function (error, result) {
+
+    var page = req.query.page;
+    var perPage = 5;
+    var start = (page - 1) * perPage;
+    var finish = page * perPage;
+    var total = result[0].count;
+    var totalPageCount = Math.ceil(total / perPage);
+    var hasNextPage =  page < totalPageCount;
+
+    var query = 'SELECT * FROM screencasts s LIMIT ' + start + ', ' + finish;
+    // switch (req.params.period) {
+    //   case 'month':
+    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
+    //     break;
+    //   case 'week':
+    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
+    //     break;
+    //   default:
+    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
+    //     break;
+    // }
+    connection.query(query, function (error, screencasts) {
+      res.json({
+        screencasts: screencasts,
+        hasMore: hasNextPage
+      });
+    });
   });
 });
 
