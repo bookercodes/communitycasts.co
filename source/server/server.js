844c0250052b81d10b30aa6e51866a8b83b173e8
Alex Booker <alexbooker@fastmail.im>
2015-07-09 18:16:20 +0100

    pagination is now relative to sort option

--- source/server/server.js
+++ source/server/server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mysql = require('mysql');
 var cors = require('cors');
-var sleep = require('sleep');
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -15,10 +14,19 @@ var app = express();
 app.use(cors());
 
 app.get('/screencasts/top/:period', function (req, res) {
-
-  connection.query('SELECT COUNT(*) AS count FROM screencasts', function (error, result) {
-
-
+  var query = 'SELECT COUNT(*) AS count FROM screencasts s';
+  switch (req.params.period) {
+    case 'month':
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
+      break;
+    case 'week':
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
+      break;
+    default:
+      query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
+      break;
+  }
+  connection.query(query, function (error, result) {
     var page = req.query.page;
     var perPage = 5;
     var start = (page - 1) * perPage;
@@ -26,19 +34,19 @@ app.get('/screencasts/top/:period', function (req, res) {
     var total = result[0].count;
     var totalPageCount = Math.ceil(total / perPage);
     var hasNextPage =  page < totalPageCount;
-
-    var query = 'SELECT * FROM screencasts s LIMIT ' + start + ', ' + finish;
-    // switch (req.params.period) {
-    //   case 'month':
-    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
-    //     break;
-    //   case 'week':
-    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
-    //     break;
-    //   default:
-    //     query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
-    //     break;
-    // }
+    var query = 'SELECT * FROM screencasts s';
+    switch (req.params.period) {
+      case 'month':
+        query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 MONTH)';
+        break;
+      case 'week':
+        query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 WEEK)';
+        break;
+      default:
+        query += ' WHERE s.submissionDate > DATE_SUB(NOW(), INTERVAL 1 DAY)';
+        break;
+    }
+    query += ' LIMIT ' + start + ', ' + finish;
     connection.query(query, function (error, screencasts) {
       res.json({
         screencasts: screencasts,
