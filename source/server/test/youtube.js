eb1de7eec8cc459161aecf7c0c66a18bc14e03c2
Alex Booker <alexbooker@fastmail.im>
2015-07-10 20:26:42 +0100

    improved test output readability a little

--- source/server/test/youtube.js
+++ source/server/test/youtube.js
@@ -4,19 +4,21 @@ var sut = require('../youtube');
 /*jshint unused:false*/
 var should = require('should');
 
-describe('isYouTubeUrl', function () {
-  it('returns correct result', function () {
-    // TODO: What about domains other than .com, should they be valid, too?
-    // Domains that start with http:// rather than https://?
-    // Domains that without www.?
-    sut.isYouTubeUrl('https://www.youtube.com/watch?v=CsGYh8AacgY').should.equal(true);
-    sut.isYouTubeUrl('https://www.youtube.com/watch?v=QFCSXr6qnv4').should.equal(true);
-    sut.isYouTubeUrl('https://www.youtube.com/watch?v=9bZkp7q19f0').should.equal(true);
-    sut.isYouTubeUrl('https://youtu.be/CsGYh8AacgY').should.equal(true);
-    sut.isYouTubeUrl('https://youtu.be/9bZkp7q19f0').should.equal(true);
+describe('youtube', function () {
+  describe('isYouTubeUrl', function () {
+    it('returns correct result', function () {
+      // TODO: What about domains other than .com, should they be valid, too?
+      // Domains that start with http:// rather than https://?
+      // Domains that without www.?
+      sut.isYouTubeUrl('https://www.youtube.com/watch?v=CsGYh8AacgY').should.equal(true);
+      sut.isYouTubeUrl('https://www.youtube.com/watch?v=QFCSXr6qnv4').should.equal(true);
+      sut.isYouTubeUrl('https://www.youtube.com/watch?v=9bZkp7q19f0').should.equal(true);
+      sut.isYouTubeUrl('https://youtu.be/CsGYh8AacgY').should.equal(true);
+      sut.isYouTubeUrl('https://youtu.be/9bZkp7q19f0').should.equal(true);
 
-    sut.isYouTubeUrl('https://communitycasts.co').should.equal(false);
-    sut.isYouTubeUrl('https://youtube.com/').should.equal(false);
-    sut.isYouTubeUrl('https://youtu.be/').should.equal(false);
-  });
+      sut.isYouTubeUrl('https://communitycasts.co').should.equal(false);
+      sut.isYouTubeUrl('https://youtube.com/').should.equal(false);
+      sut.isYouTubeUrl('https://youtu.be/').should.equal(false);
+    });
+});
 });
