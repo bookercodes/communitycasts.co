cfb3156c7f22e8bc645af0a0a555a55d7b0873f8
Alex Booker <alexbooker@fastmail.im>
2015-07-10 20:21:31 +0100

    added more youtube tests

--- source/server/test/youtube.js
+++ source/server/test/youtube.js
@@ -4,16 +4,19 @@ var sut = require('../youtube');
 /*jshint unused:false*/
 var should = require('should');
 
-describe('the isYouTubeUrl function', function () {
-  it('should return true if input is a valid YouTube link', function () {
+describe('isYouTubeUrl', function () {
+  it('returns correct result', function () {
+    // TODO: What about domains other than .com, should they be valid, too?
+    // Domains that start with http:// rather than https://?
+    // Domains that without www.?
     sut.isYouTubeUrl('https://www.youtube.com/watch?v=CsGYh8AacgY').should.equal(true);
-    sut.isYouTubeUrl('https://youtu.be/CsGYh8AacgY').should.equal(true);
     sut.isYouTubeUrl('https://www.youtube.com/watch?v=QFCSXr6qnv4').should.equal(true);
     sut.isYouTubeUrl('https://www.youtube.com/watch?v=9bZkp7q19f0').should.equal(true);
+    sut.isYouTubeUrl('https://youtu.be/CsGYh8AacgY').should.equal(true);
     sut.isYouTubeUrl('https://youtu.be/9bZkp7q19f0').should.equal(true);
-  });
-  it('should return false if input is not a valid YouTube link', function () {
-    sut.isYouTubeUrl('http://communitycasts.co').should.equal(false);
-    sut.isYouTubeUrl('http://youtube.com').should.equal(false);
+
+    sut.isYouTubeUrl('https://communitycasts.co').should.equal(false);
+    sut.isYouTubeUrl('https://youtube.com/').should.equal(false);
+    sut.isYouTubeUrl('https://youtu.be/').should.equal(false);
   });
 });
