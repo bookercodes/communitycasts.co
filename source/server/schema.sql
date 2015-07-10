523d309fa070c3a14c5c455d9d611bf17f9d3329
Alex Booker <alexbooker@fastmail.im>
2015-07-10 19:51:34 +0100

    updated database schema

--- source/server/schema.sql
+++ source/server/schema.sql
@@ -4,9 +4,26 @@ USE communityCasts;
 
 CREATE TABLE screencasts (
   screencastId        INT AUTO_INCREMENT,
-  title               NVARCHAR(200) NOT NULL,
+  link                NVARCHAR(500) NOT NULL,
+  title               NVARCHAR(500) NOT NULL,
   durationInSeconds   INT           NOT NULL,
   submissionDate      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   referralCount       INT           NOT NULL DEFAULT 0,
   PRIMARY KEY(screencastId)
+);
+
+CREATE TABLE tags (
+  tagName NVARCHAR(100),
+  PRIMARY KEY (tagName)
+);
+
+CREATE TABLE screencastTags (
+  screencastId INT,
+  tagName NVARCHAR(100),
+
+  PRIMARY KEY (screencastId, tagName),
+  FOREIGN KEY (screencastId)
+    REFERENCES screencasts(screencastId),
+  FOREIGN KEY (tagName)
+    REFERENCES tags(tagName) ON UPDATE CASCADE
 );
