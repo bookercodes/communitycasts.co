a5350932a6d1c1d27853dc58624aca94684b72a8
Alex Booker <alexbooker@fastmail.im>
2015-07-10 20:35:30 +0100

    made alert title bold

--- source/client/app/controllers/submitController.js
+++ source/client/app/controllers/submitController.js
@@ -1,8 +1,13 @@
-app.controller('submitController', function ($scope, $http, $state) {
+app.controller('submitController', function ($scope, $http, alert, $state) {
   $scope.submitScreencast = function () {
     var url = 'http://localhost:3000/screencasts';
-    $http.post(url, $scope.screencast).success(function onSuccess() {
-      $state.go('home');
-    });
+    $http.post(url, $scope.screencast)
+      .success(function onSuccess(data) {
+        alert('Success!', data.message);
+        $state.go('home');
+      })
+      .error(function onError(data) {
+        alert('Error!', data.message);
+      });
   };
 });
