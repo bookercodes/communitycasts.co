cccc0d8d05f1956434771d99a6bb986c9857c84c
Alex Booker <alexbooker@fastmail.im>
2015-07-13 18:29:54 +0100

    readability tweaks

--- source/server/youtube.js
+++ source/server/youtube.js
@@ -10,38 +10,45 @@ function Youtube(key) {
 
   this.key = key;
 
-  this.isYouTubeUrl = function (url) {
+  this.isYouTubeUrl = function(url) {
     return /^(https?\:\/\/)?(www\.)?(youtube\.com|youtu\.?be)\/.+$/.test(url);
   };
 
-  var parseIdFromUrl = function (url) {
-    var pattern = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    var match = url.match(pattern);
-    if (match && match[2].length === 11) {
-      return match[2];
-    }
-  };
-
-  this.fetchVideoDetails = function (url, callback) {
+  this.fetchVideoDetails = function(url, callback) {
     if (!this.key) {
       throw new Error('This function requires that you supply a key.');
     }
-    var id = parseIdFromUrl(url);
-    var apiUrl = 'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=' + id + '&key=' + this.key;
-    request(apiUrl, function (error, request, body) {
-      if (error) { return callback(error); }
+    var id = parseVideoId(url);
+    var apiUrl =
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=' +
+      id + '&key=' + this.key;
+    request(apiUrl, function(error, request, body) {
+      if (error) {
+        return callback(error);
+      }
       var parsed = JSON.parse(body);
       var video = parsed.items[0];
       return callback(null, {
         screencastId: id,
         title: video.snippet.title,
-        durationInSeconds: moment.duration(video.contentDetails.duration).asSeconds(),
+        durationInSeconds: moment.duration(video.contentDetails.duration)
+          .asSeconds(),
         channel: {
           name: video.snippet.channelTitle,
-       }
+        }
       });
     });
   };
+
+  // http://stackoverflow.com/a/9102270
+  function parseVideoId(url) {
+    var match = url.match(
+      /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/);
+    if (match && match[2].length === 11) {
+      return match[2];
+    }
+    return undefined;
+  }
 }
 
 module.exports = Youtube;
