5ff6b4fc8bb3be7e6a7cb90ab2795f9594505ee7
Alex Booker <alexbooker@fastmail.im>
2015-07-14 20:02:22 +0100

    added input validation test

--- source/server/test/screencastsApi.js
+++ source/server/test/screencastsApi.js
@@ -11,6 +11,22 @@ describe('screencasts route', function () {
         link: 'https://www.youtube.com/watch?v=XuLRKMqozwA',
         tags: 'JavaScript, Node, MySQL'
       })
-      .expect(201, done);
-    });
+      .expect(201)
+      .expect({
+        message: 'Thank you for your submission. Your submission will be reviewed by the moderators and if it meets our guidelines, it\'ll appear on the home page soon!'
+      }, done);
+  });
+
+  it('should return 400', function (done) {
+    request(server)
+      .post('/screencasts')
+      .send({
+        link: 'http://youtube.com/',
+        tags: 'JavaScript'
+      })
+      .expect(400)
+      .expect({
+        message: 'Please enter a valid YouTube or Vimeo video url.'
+      }, done);
+  });
 });
