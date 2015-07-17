ec01d094d600112d700fe35e9603e17c7578dca4
Alex Booker <alexbooker@fastmail.im>
2015-07-17 19:49:04 +0100

    added another integration test

--- source/server/test/foo.js
+++ source/server/test/foo.js
@@ -1,7 +1,31 @@
 'use strict';
 
+var Code = require('code');
 var request = require('supertest');
 var server = require('../server');
+var expect = Code.expect;
+
+function cleanDatabase(done) {
+  var mysql = require('mysql');
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'communityCasts'
+  });
+  connection.connect();
+  /* jshint multistr:true*/
+  var query = '\
+  SET FOREIGN_KEY_CHECKS=0; \
+  TRUNCATE TABLE screencasts; \
+  TRUNCATE TABLE tags; \
+  TRUNCATE TABLE screencastTags; \
+  TRUNCATE TABLE referrals; \
+  SET FOREIGN_KEY_CHECKS=1;';
+  connection.query(query, function () {
+    done();
+  });
+}
 
 describe('createScreencast()', function () {
   it('should return error if link is blank', function (done) {
@@ -36,4 +60,45 @@ describe('createScreencast()', function () {
       .expect(400)
       .expect({ message: 'You must specify 5 or less tags.'}, done);
   });
+  it('should return 201 on success', function (done) {
+    var screencast = {
+      link: 'https://www.youtube.com/watch?v=9bZkp7q19f0',
+      tags: 'JavaScript, Node, MySQL'
+    };
+    request(server)
+      .post('/screencasts')
+      .send(screencast)
+      .expect(201)
+      .end(function (error, response) {
+        expect(error).to.be.null();
+        expect(response.body.message).to.match(/^Thank you for your submission.*it'll appear on the home page soon!$/);
+        expect(response.body.screencastId).to.be.a.number();
+        done();
+      });
+  });
+
+});
+
+describe('redirectToScreencast()', function () {
+
+  beforeEach(cleanDatabase);
+
+  it('should redirect to screencast after submission', function (done) {
+    var screencast = {
+      link: 'https://www.youtube.com/watch?v=9bZkp7q19f0',
+      tags: 'JavaScript, Node, MySQL'
+    };
+    request(server)
+      .post('/screencasts')
+      .send(screencast)
+      .end(function (error, response) {
+        request(server)
+          .get('/screencasts/' + response.body.screencastId)
+          .expect('Location', screencast.link)
+          .expect(302, done);
+      });
+  });
+
+
+
 });
