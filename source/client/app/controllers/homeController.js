b34418299ce0f196fec5a5f1d60fe88c4d3c7f2c
alexbooker <alexbooker@fastmail.im>
2015-07-28 21:15:37 +0100

    removed duplicate code in homeController.js

--- source/client/app/controllers/homeController.js
+++ source/client/app/controllers/homeController.js
@@ -1,15 +1,18 @@
-app.controller('homeController', function ($scope, $http) {
-  $scope.page = 1;
-  $scope.screencasts = [];
-  $scope.sortOption = 'today';
-  $scope.hasMore = true;
-  $scope.busy = false;
-  $scope.loaded = false;
-  $scope.fetchScreencasts = function () {
+app.controller('homeController', function($scope, $http) {
+
+  function init() {
+    $scope.page = 1;
+    $scope.screencasts = [];
+    $scope.hasMore = true;
+    $scope.loaded = false;
+    $scope.busy = false;
+  }
+
+  $scope.fetchScreencasts = function() {
     $scope.busy = true;
     var base = 'http://localhost:3000/screencasts';
     var url = base + '/top/' + $scope.sortOption + '?page=' + $scope.page;
-    $http.get(url).success(function (response) {
+    $http.get(url).success(function(response) {
       $scope.busy = false;
       $scope.page += 1;
       $scope.hasMore = response.hasMore;
@@ -17,13 +20,13 @@ app.controller('homeController', function ($scope, $http) {
       $scope.loaded = true;
     });
   };
-  $scope.changeSortOption = function () {
-    $scope.loaded = false;
-    $scope.screencasts = [];
-    $scope.page = 1;
-    $scope.hasMore = true;
-    $scope.busy = false;
+
+  $scope.changeSortOption = function() {
+    init();
     $scope.fetchScreencasts();
   };
+
+  init();
+  $scope.sortOption = 'today';
   $scope.fetchScreencasts();
 });
