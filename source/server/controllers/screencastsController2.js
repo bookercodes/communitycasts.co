5fcbe1d2f97eb86040a37f9699868172de948881
Alex Booker <alexbooker@fastmail.im>
2015-08-02 09:19:46 +0100

    added popularity formula

--- source/server/controllers/screencastsController2.js
+++ source/server/controllers/screencastsController2.js
@@ -3,33 +3,38 @@
 var config = require('config');
 
 var screencastsController = function(connection) {
-  var sendScreencasts = function (req, res) {
+  var sendScreencasts = function(req, res) {
     var query = 'SELECT COUNT(*) AS count FROM screencasts s';
     connection.queryAsync(query).spread(function(result) {
       var total = result.shift().count;
       var page = req.query.page || 1;
-      var sort = req.query.sort || 'latest';
       var perPage = config.screencastsPerPage;
       var start = (page - 1) * perPage;
       var finish = page * perPage;
       var totalPageCount = Math.ceil(total / perPage);
       var hasNextPage = page < totalPageCount;
-      /*jshint multistr:true*/
-      var query = 'SELECT * FROM screencasts s';
-      console.log(sort);
-      if (sort === 'popular')
-        query += ' ORDER BY referralCount DESC, submissionDate';
-      else
-        query += ' ORDER BY submissionDate DESC';
-      query += ' LIMIT ' + start + ', ' + finish;
+      var query =
+        'SELECT * \
+         FROM screencasts s \
+         ORDER BY (s.referralCount)/POW(((UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(s.submissionDate))/3600)+2,1.5) DESC \
+         LIMIT ' + start + ', ' + finish;
       connection.queryAsync(query).spread(function(screencasts) {
-        res.json(screencasts);
+        screencasts = screencasts.map(function(screencast) {
+          screencast.href =
+            'http://localhost:3000/screencasts/' + screencast.screencastId;
+          delete screencast.link;
+          return screencast;
+        });
+        res.json({
+          screencasts: screencasts,
+          hasMore: hasNextPage
+        });
       });
     });
   };
   return {
     sendScreencasts: sendScreencasts
-  }
+  };
 };
 
 module.exports = screencastsController;
