8bc97b98b62a317eee6089ae6b9514cb79efafe3
Alex Booker <alexbooker@fastmail.im>
2015-08-02 09:57:20 +0100

    added tags to the screencasts api results

--- source/server/controllers/screencastsController2.js
+++ source/server/controllers/screencastsController2.js
@@ -15,18 +15,23 @@ var screencastsController = function(connection) {
       var total = result.shift().count;
       var perPage = config.screencastsPerPage;
       var sort = req.query.sort || 'popular';
-      if (sort !== 'popular' || sort !== 'latest') {
-        return res.status(400).json({
-          message: 'You input the sort option "' + sort + '" which is invalid. The sort option must be "latest" or "popular". Thank you.'
-        });
-      }
+      // if (sort !== 'popular' || sort !== 'latest') {
+      //   return res.status(400).json({
+      //     message: 'You input the sort option "' + sort + '" which is invalid. The sort option must be "latest" or "popular". Thank you.'
+      //   });
+      // }
       var start = (page - 1) * perPage;
       var finish = perPage;
       var totalPageCount = Math.ceil(total / perPage);
       var hasNextPage = page < totalPageCount;
       var query =
-        'SELECT * \
-         FROM screencasts s'
+        'SELECT \
+           s.*, \
+           GROUP_CONCAT(screencastTags.tagName) AS tags \
+         FROM screencasts s \
+         JOIN screencastTags \
+           ON s.screencastId = screencastTags.screencastId \
+         GROUP BY s.screencastId';
       if(sort === 'popular')
         query += ' ORDER BY (s.referralCount)/POW(((UNIX_TIMESTAMP(NOW())-UNIX_TIMESTAMP(s.submissionDate))/3600)+2,1.5) DESC';
       else
@@ -36,6 +41,7 @@ var screencastsController = function(connection) {
         screencasts = screencasts.map(function(screencast) {
           screencast.href =
             'http://localhost:3000/screencasts/' + screencast.screencastId;
+          screencast.tags = screencast.tags.split(',');
           delete screencast.link;
           return screencast;
         });
