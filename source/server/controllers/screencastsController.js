2ab3e288f4594e34882c4da64459d5a161a9eaac
Alex Booker <alexbooker@fastmail.im>
2015-08-05 10:22:18 +0100

    added pagination for screencast tags end-point

--- source/server/controllers/screencastsController.js
+++ source/server/controllers/screencastsController.js
@@ -5,30 +5,66 @@ var config = require('config');
 module.exports = function(connection) {
 
   function sendScreencastsWithTag(req, res) {
+      var page = req.query.page || 1;
     /* jshint multistr:true */
-    var query =
+    var totalQuery =
       'SELECT \
-    	   screencasts.*, \
-         GROUP_CONCAT(screencastTags.tagName) AS tags \
-       FROM screencasts \
-       JOIN screencastTags \
-    	   ON screencasts.screencastId = screencastTags.screencastId \
-       WHERE screencasts.screencastId IN ( \
-    	   SELECT screencastId \
-           FROM screencastTags \
-           WHERE screencasttags.tagName = ' + connection.escape(req.params.tag) + ' \
-       ) \
-       GROUP BY screencasts.screencastId';
-    connection.queryAsync(query).spread(function (screencasts) {
-      screencasts = screencasts.map(function(screencast) {
-        screencast.href =
-          'http://localhost:3000/screencasts/' + screencast.screencastId;
-        screencast.tags = screencast.tags.split(',');
-        delete screencast.link;
-        return screencast;
-      });
-      res.json({
-        screencasts: screencasts
+         COUNT(*) AS total \
+       FROM ( \
+    	   SELECT \
+    		   screencasts.screencastId \
+           FROM screencasts \
+    	   JOIN screencastTags \
+    		   ON screencasts.screencastId = screencastTags.screencastId \
+    	   WHERE screencasts.screencastId IN ( \
+    		   SELECT screencastId \
+    		   FROM screencastTags \
+    		   WHERE screencasttags.tagName = ' + connection.escape(req.params.tag) + ' \
+    	   ) \
+    	   GROUP BY screencasts.screencastId \
+       ) AS result';
+    connection.queryAsync(totalQuery).spread(function(result) {
+      var total = result.shift().total;
+      var perPage = config.screencastsPerPage;
+      var sort = req.query.sort || 'popular';
+      if (sort !== 'popular' && sort !== 'latest') {
+        return res.status(400).json({
+          message: 'You input the sort option "' + sort + '" which is invalid. The sort option must be "latest" or "popular". Thank you.'
+        });
+      }
+      var start = (page - 1) * perPage;
+      var finish = perPage;
+      var totalPageCount = Math.ceil(total / perPage);
+      var hasNextPage = page < totalPageCount;
+
+      /* jshint multistr:true */
+      var query =
+        'SELECT \
+      	   screencasts.*, \
+           GROUP_CONCAT(screencastTags.tagName) AS tags \
+         FROM screencasts \
+         JOIN screencastTags \
+      	   ON screencasts.screencastId = screencastTags.screencastId \
+         WHERE screencasts.screencastId IN ( \
+      	   SELECT screencastId \
+             FROM screencastTags \
+             WHERE screencasttags.tagName = ' + connection.escape(req.params.tag) + ' \
+         ) \
+         GROUP BY screencasts.screencastId';
+      query += ' LIMIT ' + start + ', ' + finish;
+      connection.queryAsync(query).spread(function (screencasts) {
+        screencasts = screencasts.map(function(screencast) {
+          screencast.href =
+            'http://localhost:3000/screencasts/' + screencast.screencastId;
+          screencast.tags = screencast.tags.split(',');
+          delete screencast.link;
+          return screencast;
+        });
+        res.json({
+          screencasts: screencasts,
+          hasMore: hasNextPage,
+          totalCount: total
+        });
       });
     });
   }
