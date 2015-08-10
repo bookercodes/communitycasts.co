d16afe0f32dc685232c7134ea69869fddda0da17
Alex Booker <alexbooker@fastmail.im>
2015-08-10 19:38:18 +0100

    added load more button to tag cloud

--- source/client/app/controllers/navigationController.js
+++ source/client/app/controllers/navigationController.js
@@ -1,5 +1,7 @@
 app.controller('navigationController', function($scope, $http) {
 
+  $scope.tagLimit = 2;
+
   $http.get('http://localhost:3000/tags').success(function(response) {
     $scope.tags = response.tags;
   });
