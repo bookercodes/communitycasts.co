344f39cdaf27c8a532397eea2f43cea59155b2ea
Alex Booker <alexbooker@fastmail.im>
2015-08-24 15:23:29 +0100

    gutted client app

--- source/client/app/controllers/navigationController.js
+++
@@ -1,8 +0,0 @@
-app.controller('navigationController', function($scope, $http) {
-
-  $scope.tagLimit = 2;
-
-  $http.get('http://localhost:3000/tags').success(function(response) {
-    $scope.tags = response.tags;
-  });
-});
--- source/client/app/services/alertService.js
+++
@@ -1,8 +0,0 @@
-app.service('alert', function ($rootScope) {
-  return function (title, message) {
-    $rootScope.alert = {
-      title: title,
-      message: message
-    };
-  };
-});
