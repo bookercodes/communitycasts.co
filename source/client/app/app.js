f6ba2ef5303162ac01f5c62bf0d570192f6ecf45
Alex Booker <alexbooker@fastmail.im>
2015-09-01 12:40:43 +0100

    cleaned up app.js a little

--- source/client/app/app.js
+++ source/client/app/app.js
@@ -1,25 +1,27 @@
-var app = angular.module('communityCasts', ['ngMaterial', 'ui.router', 'config']);
-
-app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
-  $stateProvider.state('home', {
-    url: '/',
-    templateUrl: '/app/views/_screencasts.html',
-    controller: 'screencastsController',
-    params: {
-      sort: 'popular',
-      tagged: '',
-    },
-    data : { pageTitle: 'Community Casts' }
+var app = angular.module('communityCasts', ['ngMaterial', 'ui.router', 'config'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider.state('home', {
+      url: '/',
+      templateUrl: '/app/views/_screencasts.html',
+      controller: 'screencastsController',
+      params: {
+        sort: 'popular',
+        tagged: '',
+      },
+      data : {
+        pageTitle: 'Community Casts'
+      }
+    });
+    $stateProvider.state('about', {
+      url: '/about',
+      templateUrl: '/app/views/_about.html',
+      data : {
+        pageTitle: 'About Community Casts'
+      }
+    });
+  })
+  .run(function ($rootScope, $state, $stateParams) {
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
   });
-  $stateProvider.state('about', {
-    url: '/about',
-    templateUrl: '/app/views/_about.html',
-    data : { pageTitle: 'About Community Casts' }
-  });
-})
-.run([ '$rootScope', '$state', '$stateParams',
-function ($rootScope, $state, $stateParams) {
-  $rootScope.$state = $state;
-  $rootScope.$stateParams = $stateParams;
-}]);
