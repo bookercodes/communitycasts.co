fb032cb4e907225adc74cc490a1a8e3c034d2763
Alex Booker <alexbooker@fastmail.im>
2015-09-02 13:54:09 +0100

    updated schema and test data

--- source/server/sample-data.sql
+++ source/server/sample-data.sql
@@ -1,50 +1,44 @@
--- sample data used for development purposes ^_^
-
 INSERT INTO tags
  VALUES ('JavaScript'), ('Angular'), ('Python');
-INSERT INTO channels (channelName, channelLink)
-  VALUES ('Derek Banas', 'https://www.youtube.com/channel/UCwRXb5dUK4cvsHbx-rGzSgw');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=_cLvpJY2deo','JavaScript Video Tutorial Pt 1', 1891, 'YouTube', 1, 'https://i.ytimg.com/vi/_cLvpJY2deo/mqdefault.jpg');
-INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(), 'JavaScript');
+INSERT INTO channels (channelYoutubeId, channelName)
+  VALUES ('derekbanas', 'Derek Banas');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=QETUuZ27N0w','Angularjs Tutorial for Beginners - learn Angular.js using UI-Router',0,'YouTube', 1, 'https://i.ytimg.com/vi/QETUuZ27N0w/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('_cLvpJY2deo', 'JavaScript Video Tutorial Pt 1', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Angular');
-INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'JavaScript');
+	VALUES (LAST_INSERT_ID(), 'JavaScript');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://youtu.be/PMfcsYzj-9M','The Definitive Guide to Object-Oriented JavaScript', 1626,'YouTube', 1, 'https://i.ytimg.com/vi/PMfcsYzj-9M/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('QETUuZ27N0w', 'Angularjs Tutorial for Beginners - learn Angular.js using UI-Router', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'JavaScript');
+	VALUES (LAST_INSERT_ID(), 'Angular');
+INSERT INTO screencastTags
+	VALUES (LAST_INSERT_ID(), 'JavaScript');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=N4mEzFDjqtA','Python Programming', 49, 'YouTube', 1, 'https://i.ytimg.com/vi/N4mEzFDjqtA/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('PMfcsYzj-9M', 'he Definitive Guide to Object-Oriented JavaScript', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Python');
+	VALUES (LAST_INSERT_ID(), 'JavaScript');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=9uq3w6JJS00','Zero to Hero with Python', 39996, 'YouTube', 1, 'https://i.ytimg.com/vi/9uq3w6JJS00/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('N4mEzFDjqtA', 'Python Programming', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Python');
+	VALUES (LAST_INSERT_ID(), 'Python');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=cpPG0bKHYKc','Python Beginner Tutorial 1 (For Absolute Beginners)', 545, 'YouTube', 1, 'https://i.ytimg.com/vi/cpPG0bKHYKc/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('9uq3w6JJS00', 'Zero to Hero with Python', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Python');
+	VALUES (LAST_INSERT_ID(), 'Python');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://www.youtube.com/watch?v=0r5QvzjjKDc','AngularJS Directives Tutorial', 1139, 'YouTube', 1, 'https://i.ytimg.com/vi/0r5QvzjjKDc/mqdefault.jpg');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('cpPG0bKHYKc', 'Python Beginner Tutorial 1 (For Absolute Beginners)', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'JavaScript');
-INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Angular');
+	VALUES (LAST_INSERT_ID(), 'Python');
 
-INSERT INTO screencasts(link,title, durationInSeconds, hostService, channelId, thumbUrl)
- VALUES ('https://vimeo.com/42848594', 'Building Maya Interfaces with Python: Video One', 3938, 'Vimeo', 1, 'https://i.vimeocdn.com/video/297371276_590x332.webp');
+INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
+	VALUES ('0r5QvzjjKDc', 'AngularJS Directives Tutorial', 1891, 1);
 INSERT INTO screencastTags
- VALUES (LAST_INSERT_ID(),'Python');
+	VALUES (LAST_INSERT_ID(),'JavaScript');
+INSERT INTO screencastTags
+	VALUES (LAST_INSERT_ID(),'Angular');
--- source/server/schema.sql
+++ source/server/schema.sql
@@ -2,43 +2,32 @@ DROP DATABASE IF EXISTS communityCasts;
 CREATE DATABASE communityCasts;
 USE communityCasts;
 
-CREATE TABLE hostServices (
-  hostService NVARCHAR(100) NOT NULL,
-  PRIMARY KEY (hostService)
-);
-INSERT INTO hostServices
-  VALUES ('Vimeo'), ('YouTube');
-
 CREATE TABLE channels (
-  channelId   INT AUTO_INCREMENT,
-  channelName NVARCHAR(500) NOT NULL,
-  channelLink NVARCHAR(500) NOT NULL,
+  channelId        INT AUTO_INCREMENT,
+  channelYoutubeId NVARCHAR(200) NOT NULL UNIQUE,
+  channelName      NVARCHAR(500) NOT NULL,
 
   PRIMARY KEY (channelId)
 );
 
 CREATE TABLE screencasts (
   screencastId        INT AUTO_INCREMENT,
-  link                NVARCHAR(500) NOT NULL,
+  youtubeId           NVARCHAR(200) NOT NULL UNIQUE,
   title               NVARCHAR(500) NOT NULL,
   durationInSeconds   INT           NOT NULL,
   submissionDate      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   referralCount       INT           NOT NULL DEFAULT 0,
-  hostService         NVARCHAR(100) NOT NULL,
   channelId           INT           NOT NULL,
-  thumbUrl            NVARCHAR(500) NOT NULL,
+  approved            BOOL          NOT NULL DEFAULT 0,
   PRIMARY KEY (screencastId),
   FOREIGN KEY (channelId)
-    REFERENCES channels(channelId),
-  FOREIGN KEY (hostService)
-    REFERENCES hostServices(hostService)
+    REFERENCES channels(channelId)
 );
 
 CREATE TABLE tags (
   tagName NVARCHAR(100),
   PRIMARY KEY (tagName)
 );
-
 
 CREATE TABLE screencastTags (
   screencastId INT,
