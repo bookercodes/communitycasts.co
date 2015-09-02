fdd7ec538a7647094203df234d1b856d3f252de3
Alex Booker <alexbooker@fastmail.im>
2015-09-02 16:46:11 +0100

    removed identity columns

--- source/server/sample-data.sql
+++ source/server/sample-data.sql
@@ -1,44 +1,44 @@
 INSERT INTO tags
  VALUES ('JavaScript'), ('Angular'), ('Python');
 
-INSERT INTO channels (channelYoutubeId, channelName)
+INSERT INTO channels (channelId, channelName)
   VALUES ('derekbanas', 'Derek Banas');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('_cLvpJY2deo', 'JavaScript Video Tutorial Pt 1', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('_cLvpJY2deo', 'JavaScript Video Tutorial Pt 1', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'JavaScript');
+	VALUES ('_cLvpJY2deo', 'JavaScript');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('QETUuZ27N0w', 'Angularjs Tutorial for Beginners - learn Angular.js using UI-Router', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('QETUuZ27N0w', 'Angularjs Tutorial for Beginners - learn Angular.js using UI-Router', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'Angular');
+	VALUES ('QETUuZ27N0w', 'Angular');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'JavaScript');
+	VALUES ('QETUuZ27N0w', 'JavaScript');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('PMfcsYzj-9M', 'he Definitive Guide to Object-Oriented JavaScript', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('PMfcsYzj-9M', 'The Definitive Guide to Object-Oriented JavaScript', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'JavaScript');
+	VALUES ('PMfcsYzj-9M', 'JavaScript');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('N4mEzFDjqtA', 'Python Programming', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('N4mEzFDjqtA', 'Python Programming', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'Python');
+	VALUES ('N4mEzFDjqtA', 'Python');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('9uq3w6JJS00', 'Zero to Hero with Python', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('9uq3w6JJS00', 'Zero to Hero with Python', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'Python');
+	VALUES ('9uq3w6JJS00', 'Python');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('cpPG0bKHYKc', 'Python Beginner Tutorial 1 (For Absolute Beginners)', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('cpPG0bKHYKc', 'Python Beginner Tutorial 1 (For Absolute Beginners)', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(), 'Python');
+	VALUES ('cpPG0bKHYKc', 'Python');
 
-INSERT INTO screencasts(youtubeId,title, durationInSeconds, channelId)
-	VALUES ('0r5QvzjjKDc', 'AngularJS Directives Tutorial', 1891, 1);
+INSERT INTO screencasts(screencastId,title, durationInSeconds, channelId)
+	VALUES ('0r5QvzjjKDc', 'AngularJS Directives Tutorial', 1891, 'derekbanas');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(),'JavaScript');
+	VALUES ('0r5QvzjjKDc','JavaScript');
 INSERT INTO screencastTags
-	VALUES (LAST_INSERT_ID(),'Angular');
+	VALUES ('0r5QvzjjKDc','Angular');
--- source/server/schema.sql
+++ source/server/schema.sql
@@ -3,21 +3,19 @@ CREATE DATABASE communityCasts;
 USE communityCasts;
 
 CREATE TABLE channels (
-  channelId        INT AUTO_INCREMENT,
-  channelYoutubeId NVARCHAR(200) NOT NULL UNIQUE,
-  channelName      NVARCHAR(500) NOT NULL,
+  channelId   NVARCHAR(200) NOT NULL UNIQUE,
+  channelName NVARCHAR(500) NOT NULL,
 
   PRIMARY KEY (channelId)
 );
 
 CREATE TABLE screencasts (
-  screencastId        INT AUTO_INCREMENT,
-  youtubeId           NVARCHAR(200) NOT NULL UNIQUE,
+  screencastId        NVARCHAR(200) NOT NULL UNIQUE,
   title               NVARCHAR(500) NOT NULL,
   durationInSeconds   INT           NOT NULL,
   submissionDate      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
   referralCount       INT           NOT NULL DEFAULT 0,
-  channelId           INT           NOT NULL,
+  channelId           NVARCHAR(200) NOT NULL,
   approved            BOOL          NOT NULL DEFAULT 0,
   PRIMARY KEY (screencastId),
   FOREIGN KEY (channelId)
@@ -30,7 +28,7 @@ CREATE TABLE tags (
 );
 
 CREATE TABLE screencastTags (
-  screencastId INT,
+  screencastId NVARCHAR(200),
   tagName NVARCHAR(100),
 
   PRIMARY KEY (screencastId, tagName),
@@ -41,7 +39,7 @@ CREATE TABLE screencastTags (
 );
 
 CREATE TABLE referrals (
-  screencastId INT,
+  screencastId NVARCHAR(200),
   refereeRemoteAddress NVARCHAR(20) NOT NULL,
 
   PRIMARY KEY (screencastId, refereeRemoteAddress),
