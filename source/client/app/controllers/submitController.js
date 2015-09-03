e47703d1d286b4daecb80e04a9823e03759236d3
Alex Booker <alexbooker@fastmail.im>
2015-09-03 14:32:55 +0100

    got md-chips working

--- source/client/app/controllers/submitController.js
+++ source/client/app/controllers/submitController.js
@@ -1,8 +1,10 @@
 app.controller('submitController', ['$scope', function($scope) {
   'use strict';
+  $scope.screencast = {};
+  $scope.screencast.tags = ['Angular'];
 
-  $scope.message = 'Foo';
-
-  // $scope.screencast.tags = ['Angular'];
+  $scope.submit = function () {
+    alert(JSON.stringify($scope.screencast));
+  };
 
 }]);
