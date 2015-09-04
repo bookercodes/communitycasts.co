5fbc1bf42623e1da42112f1ed690e2c0f285c925
Alex Booker <alexbooker@fastmail.im>
2015-09-04 12:53:52 +0100

    made tag input a text input instead of chips control

--- source/client/app/controllers/submitController.js
+++ source/client/app/controllers/submitController.js
@@ -2,12 +2,9 @@ app.controller('submitController', ['$scope', '$http', function($scope, $http) {
   'use strict';
 
   $scope.screencast = {};
-  $scope.screencast.tags = [];
 
   $scope.submit = function () {
-    var copy = $scope.screencast;
-    copy.tags = copy.tags.join();
-    $http.post('/screencasts', copy).then(function () {
+    $http.post('/screencasts', $scope.screencast).then(function () {
       alert('yay');
     });
   };
