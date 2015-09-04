90cc04d51b5bbe05dd5d201bafa0f70aa8815d9e
Alex Booker <alexbooker@fastmail.im>
2015-09-04 15:24:33 +0100

    show toast and redirect user on successful submission

--- source/client/app/controllers/submitController.js
+++ source/client/app/controllers/submitController.js
@@ -1,11 +1,17 @@
-app.controller('submitController', ['$scope', '$http', function($scope, $http) {
+app.controller('submitController', ['$scope', '$mdToast', '$http', '$state', function($scope, $mdToast, $http, $state) {
   'use strict';
 
   $scope.screencast = {};
 
   $scope.submit = function () {
     $http.post('/screencasts', $scope.screencast).then(function () {
-      alert('yay');
+      $mdToast.show(
+        $mdToast.simple()
+          .content('Thank you for your submission. Your submission will be reviewed by the moderators and if it meets our guidelines, it\'ll appear on the home page soon!')
+          .position('top right')
+          .hideDelay(3000)
+      );
+      $state.go('home');
     });
   };
 
