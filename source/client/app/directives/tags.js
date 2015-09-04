ee09736d7836d10e71f8c9959e45f86e511edc45
Alex Booker <alexbooker@fastmail.im>
2015-09-04 14:06:48 +0100

    added tags validation directive

---
+++ source/client/app/directives/tags.js
@@ -0,0 +1,20 @@
+app.directive('tags', function() {
+  'use strict';
+  return {
+    require: 'ngModel',
+    link: function(scope, element, attrs, ngModel) {
+      ngModel.$parsers.push(function(value) {
+
+        var tags = value.split(',');
+        tags = tags.filter(function (tag) {
+            return /\S/.test(tag);
+        });
+        tags = tags.filter(function (tag, pos, self) {
+          return self.indexOf(tag) === pos;
+        });
+        ngModel.$setValidity('tooManyTags', tags.length < 5);
+        return value;
+      });
+    }
+  };
+});
