a53c85b318a5b055f31b478844d4feeefe41bf78
Alex Booker <alexbooker@fastmail.im>
2015-09-12 00:56:09 +0100

    cleaned up menuController.js

--- source/client/app/controllers/menuController.js
+++ source/client/app/controllers/menuController.js
@@ -4,9 +4,9 @@
     $http.get(config.serverHost + 'api/tags').success(function(response) {
       $scope.tags = response.tags;
     });
-    $scope.custom = true;
+    $scope.menuVisible = false;
     $scope.toggleCustom = function() {
-      $scope.custom = $scope.custom === false ? true : false;
+      $scope.menuVisible = !$scope.menuVisible;
     };
   };
   menuController.$inject = ['$scope', '$http', '$location', 'config'];
