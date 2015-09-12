7454dc6d506acbd9c634da71c00e6f4ba10c2776
Alex Booker <alexbooker@fastmail.im>
2015-09-12 20:59:32 +0100

    fixed discover screencasts bug

--- source/client/app/controllers/heroController.js
+++ source/client/app/controllers/heroController.js
@@ -2,8 +2,9 @@
   'use strict';
   var heroController = function($scope, smoothScroll, $state) {
     $scope.scrollToScreencasts = function() {
-      $state.go('home');
-      smoothScroll(document.getElementById('main'));
+      $state.go('home').then(function () {
+        smoothScroll(document.getElementById('main'));
+      });
     };
   };
   heroController.$inject = ['$scope', 'smoothScroll', '$state'];
