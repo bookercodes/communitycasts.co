01aca7841e0f12ca60c70bf3f0e73445a77347c0
Alex Booker <alexbooker@fastmail.im>
2015-09-16 19:52:45 +0100

    added channels table definition

--- source/server/models/index.js
+++ source/server/models/index.js
@@ -5,6 +5,18 @@ var sequelize = new Sequelize('communityCasts', 'root', config.databasePassword,
   host: 'localhost',
 });
 
+var Channels = sequelize.define('channels', {
+  channelId: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  channelName: {
+    type: Sequelize.STRING,
+  }
+}, {
+  timestamps: false
+});
+
 var Screencasts =  sequelize.define('screencasts', {
   screencastId: {
     type: Sequelize.STRING,
@@ -64,9 +76,11 @@ var ScreencastTags = sequelize.define('screencasttags', {
 Screencasts.belongsToMany(Tags, { through:ScreencastTags, foreignKey: 'screencastId' });
 Tags.belongsToMany(Screencasts, { through:ScreencastTags, foreignKey:'tagName' });
 ScreencastTags.belongsTo(Screencasts, { foreignKey: 'screencastId' });
+Screencasts.belongsTo(Channels, { foreignKey:'channelId' });
 
 module.exports = {
   Screencasts: Screencasts,
   Tags: Tags,
-  ScreencastTags: ScreencastTags
+  ScreencastTags: ScreencastTags,
+  Channels: Channels
 };
