04d8df0240d624b663937357e4b0e28451f7ec43
Alex Booker <alexbooker@fastmail.im>
2015-09-16 11:42:56 +0100

    setup sequelize orm

--- source/server/controllers/tagsController.js
+++ source/server/controllers/tagsController.js
@@ -1,25 +1,28 @@
 'use strict';
-var squel = require('squel');
+// var squel = require('squel');
 var winston = require('winston');
+var models = require('../models');
+var Sequelize = require('Sequelize');
 
-module.exports = function(connection) {
+module.exports = function() {
   return {
     send20Tags: function (req, res) {
       winston.info('Sending tags.');
-      var sql = squel.select()
-        .from('tags')
-        .field('tags.tagName')
-        .field('count(*) as count')
-        .join('screencastTags', null, 'screencastTags.tagName = tags.tagName')
-        .join('screencasts', null, 'screencasts.screencastId = screencastTags.screencastId')
-        .where('screencasts.approved = 1')
-        .group('tags.tagName')
-        .order('count', false)
-        .limit(20)
-        .toString();
-        connection.queryAsync(sql).spread(function(tags) {
-          res.json({tags:tags});
-        });
+
+      models.ScreencastTags.findAll({
+        group: ['tagName'],
+        attributes: [
+          'tagName',
+          [Sequelize.fn('count', 'tagName'), 'count']
+        ],
+        include: [{
+          model: models.Screencasts,
+          where: { approved: true },
+          attributes: []
+        }]
+      }).then(function (screencasts) {
+        res.json(screencasts);
+      });
     }
   };
 };
---
+++ source/server/models/index.js
@@ -0,0 +1,72 @@
+var Sequelize = require('Sequelize');
+var config = require('config');
+
+var sequelize = new Sequelize('communityCasts', 'root', config.databasePassword, {
+  host: 'localhost',
+});
+
+var Screencasts =  sequelize.define('screencasts', {
+  screencastId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  durationInSeconds: {
+    type: Sequelize.INTEGER(11),
+  },
+  description: {
+    type: Sequelize.STRING,
+  },
+  submissionDate: {
+    type: Sequelize.DATE,
+  },
+  referralCount: {
+    type: Sequelize.INTEGER(11),
+  },
+  channelId: {
+    type: Sequelize.STRING,
+  },
+  approved: {
+    type: Sequelize.BOOLEAN,
+  },
+  featured: {
+    type: Sequelize.BOOLEAN,
+  }
+}, {
+  timestamps: false
+});
+
+var Tags = sequelize.define('tags', {
+  tagName: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  }
+}, {
+  timestamps: false
+});
+
+var ScreencastTags = sequelize.define('screencasttags', {
+  screencastId: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  },
+  tagName: {
+    type: Sequelize.STRING,
+    primaryKey: true
+  }
+}, {
+  timestamps: false
+});
+
+Screencasts.belongsToMany(Tags, { through:ScreencastTags, foreignKey: 'screencastId' });
+Tags.belongsToMany(Screencasts, { through:ScreencastTags, foreignKey:'tagName' });
+ScreencastTags.belongsTo(Screencasts, { foreignKey: 'screencastId' });
+
+module.exports = {
+  Screencasts: Screencasts,
+  Tags: Tags,
+  ScreencastTags: ScreencastTags
+};
