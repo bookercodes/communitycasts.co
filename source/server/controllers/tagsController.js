4b00b779a36fc4f574dafda0933eee51a7c84954
Alex Booker <alexbooker@fastmail.im>
2015-09-16 11:54:53 +0100

    refactored tags api to use sequelize

--- source/server/controllers/tagsController.js
+++ source/server/controllers/tagsController.js
@@ -8,20 +8,21 @@ module.exports = function() {
   return {
     send20Tags: function (req, res) {
       winston.info('Sending tags.');
-
       models.ScreencastTags.findAll({
         group: ['tagName'],
+        limit: 20,
         attributes: [
           'tagName',
           [Sequelize.fn('count', 'tagName'), 'count']
         ],
+        order: 'count DESC',
         include: [{
           model: models.Screencasts,
           where: { approved: true },
           attributes: []
         }]
       }).then(function (screencasts) {
-        res.json(screencasts);
+        res.json({ tags: screencasts });
       });
     }
   };
