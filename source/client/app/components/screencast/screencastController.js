601355be538131f81c00689e4e70dcc5ae9baf7a
Alex Booker <alexbooker@fastmail.im>
2015-11-18 19:15:38 +0000

    cleaned up player markup

--- source/client/app/components/screencast/screencastController.js
+++ source/client/app/components/screencast/screencastController.js
@@ -1,11 +1,18 @@
 (function() {
   'use strict';
   var screencastController = function($scope, $stateParams, screencast) {
+    $scope.playerVars = {
+      enablejsapi: 1,
+      autoplay: 1,
+      theme: 'light',
+      color: 'white',
+      origin: 'communitycasts.co'
+    };
     screencast($stateParams.screencastId).then(function(screencast) {
       $scope.screencast = screencast.data;
     });
   };
-  screencastController.$inject = ['$scope','$stateParams', 'screencast'];
+  screencastController.$inject = ['$scope', '$stateParams', 'screencast'];
   angular
     .module('communityCasts')
     .controller('screencastController', screencastController);
