18ef04839e53d17dc9815d7ad124e889f7571270
Alex Booker <alexbooker@fastmail.im>
2015-12-03 09:54:32 +0000

    added cron shell

--- source/server/cron/tweeter.js
+++ source/server/cron/tweeter.js
@@ -1,7 +1,12 @@
 'use strict';
+import {CronJob} from 'cron';
+
+function onTick() {
+  console.log('You will see this message every second');
+}
 
 export default {
   start() {
-    console.log('started cron');
+    new CronJob('* * * * * *', onTick, null, true, 'Europe/London');
   }
 };
