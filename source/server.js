2648fa59f1eb79cf94b29a5691a85d260176052c
Alex Booker <alexbooker@fastmail.im>
2016-03-30 22:28:18 +0100

    Setup foundation for integration tests

--- source/server.js
+++ source/server.js
@@ -1,11 +1,20 @@
 // @flow
 
 import express from 'express'
+import path from 'path'
+import db from 'sequelize-connect'
+import bodyParser from 'body-parser'
 
-const app: any = express()
+let app: any = express()
+app.use(bodyParser.json())
 
 app.get('*', (req, res) => res.json('hello'))
 
-app.listen(3000)
+app.post('/api/screencasts', async (req, res) => {
+  await db.models.Screencast.create({
+    url: req.body.url
+  })
+  res.sendStatus(201)
+})
 
 export default app
--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,44 @@
 // @flow
 
+import db from 'sequelize-connect'
 import test from 'ava'
+import path from 'path'
 import request from 'supertest-as-promised'
 import server from '../source'
+import config from '../source/config/config.json'
 
-test(async t => {
+test.before(function initializeDb() {
+  db.matcher = () => true
+  db.discover = [path.join(__dirname, '../source/models')]
+  const { database, username, password } = config.test
+  return db.connect(database, username, password, {
+    force: true
+  })
+})
+
+test('POST to /api/screencasts with valid screencast should return status 201', async t => {
+  const response = await request(server)
+    .post('/api/screencasts')
+    .send({
+      url: 'https://www.youtube.com/watch?v=qsDvJrGMSUY'
+    })
+  const actual = response.statusCode
+
+  t.true(actual === 201)
+})
+
+test('POST to /api/screencasts with valid screencast should store screencast in db', async t => {
+  const screencastUrl = 'https://www.youtube.com/watch?v=qsDvJrGMSUY'
   await request(server)
-    .get('/')
-    .expect(200)
+    .post('/api/screencasts')
+    .send({
+      url: screencastUrl
+    })
+  const actual = await db.models.Screencast.findOne({
+    where: {
+      url: screencastUrl
+    }
+  })
+
+  t.true(actual !== null)
 })
