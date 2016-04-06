9c235b366237f5c79e1624eaf2c0246a2c3c1b8f
Adnan B <adnanb@Adnans-MacBook-Pro.local>
2016-04-06 14:21:15 +0200

    errors.js -> statusCodes.js - Started extending statusCodes

---
+++ source/util/statusCodes.js
@@ -0,0 +1,14 @@
+export default {
+	// authentication
+	authHeaderMiss:	'Authorization header missing',
+	authHeaderInv:	'Invalid Authorization header',
+	passwordInv:	'Invalid password',
+
+	// validation
+	reqBodyMiss:	'Request body is missing',
+	urlInv:			'The Requested URL is invalid',
+	tagsMiss:		'There are some Tags missing',
+
+	// screencast
+	castNotFound:	'The Screencast cannot be found'
+}
