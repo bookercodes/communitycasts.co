64110a19823b68fe81d6b50e221758368461720c
Alex Booker <alexbooker@fastmail.im>
2016-05-17 17:23:53 +0100

    Move screencast query code to screencast model

--- source/controllers/screencastsController.js
+++ source/controllers/screencastsController.js
@@ -8,36 +8,14 @@ import commaSplit from 'comma-split'
 const screencastsController = { }
 
 screencastsController.handlePost = async (req: any, res: any, next: any) : any => {
-  const youtube = createYoutubeClient(config.youtubeApiKey)
   try {
-    const videoDetails = await youtube.fetchVideoDetails(req.body.url)
-    const tags = commaSplit(req.body.tags).map(tag => {
+    const youtube = createYoutubeClient(config.youtubeApiKey)
+    const screencast = await youtube.fetchVideoDetails(req.body.url)
+    screencast.tags = commaSplit(req.body.tags).map(tag => {
       return { id: tag }
     })
-    await db.sequelize.transaction(async transaction => {
-      const createdChannel = await db.models.channel.create({
-        id: videoDetails.channel.id,
-        name: videoDetails.channel.title
-      })
-      await createdChannel.createScreencast({
-        ...videoDetails,
-        url: req.body.url
-      }, {
-        transaction
-      })
-      await db.models.tag.bulkCreate(tags, {
-        transaction
-      })
-      const screencastTags = tags.map(tag => {
-        return {
-          screencastId: videoDetails.id,
-          tagId: tag.id
-        }
-      })
-      await db.models.screencastTag.bulkCreate(screencastTags, {
-        transaction
-      })
-    })
+    screencast.url = req.body.url
+    await db.models.screencast.createScreencast(screencast)
     res.sendStatus(201)
   } catch (error) {
     next(error)
@@ -46,32 +24,24 @@ screencastsController.handlePost = async (req: any, res: any, next: any) : any =
 
 screencastsController.handleGet = async (req: any, res: any, next: any) => {
   try {
-    const foundScreencasts = await db
-      .models
-      .screencast
-      .findAll({
-        include: [
-          { model: db.models.channel },
-          { model: db.models.tag }
-        ]
-      })
-      .map(screencast => {
-        screencast = screencast.dataValues
-        return {
-          id: screencast.id,
-          url: screencast.url,
-          title: screencast.title,
-          description: screencast.description,
-          durationInSeconds: screencast.durationInSeconds,
-          channel: {
-            id: screencast.channel.dataValues.id,
-            name: screencast.channel.dataValues.name
-          },
-          tags: screencast.tags.map(tag => tag.dataValues.id)
-        }
-      })
+    const foundScreencasts = (await db.models.screencast.findAllScreencasts()).map(screencast => {
+      screencast = screencast.dataValues
+      return {
+        id: screencast.id,
+        url: screencast.url,
+        title: screencast.title,
+        description: screencast.description,
+        durationInSeconds: screencast.durationInSeconds,
+        channel: {
+          id: screencast.channel.dataValues.id,
+          name: screencast.channel.dataValues.name
+        },
+        tags: screencast.tags.map(tag => tag.dataValues.id)
+      }
+    })
     res.status(200).json(foundScreencasts)
   } catch (err) {
+    console.log(err)
     next(err)
   }
 }
--- source/models/screencast.js
+++ source/models/screencast.js
@@ -1,14 +1,8 @@
 // @flow
+import db from 'sequelize-connect'
 
-/**
- * Creates and returns a Sequelize screencast model. (This function is to be
- * called internally by the sequelize-connect module.)
- *
- * @param sequelize - A Sequelize connection
- * @param DataTypes - A quasi enumeration of SQL data types
- */
-const createScreencastModel = function (sequelize: any, DataTypes: any) : any {
-  const screencast: any = sequelize.define('screencast', {
+const createScreencastModel = (sequelize: any, DataTypes: any) : any => {
+  const screencast = sequelize.define('screencast', {
     id: {
       primaryKey: true,
       type: DataTypes.STRING
@@ -19,11 +13,47 @@ const createScreencastModel = function (sequelize: any, DataTypes: any) : any {
     durationInSeconds: DataTypes.INTEGER
   }, {
     classMethods: {
-      associate: function (models: any): any {
+      associate (models: any): any {
         screencast.belongsTo(models.channel)
         screencast.belongsToMany(models.tag, {
           through: models.screencastTag,
           foreignKey: 'screencastId'
+        })
+      },
+
+      async findAllScreencasts (): any {
+        return screencast
+          .findAll({
+            include: [
+              { model: db.models.channel },
+              { model: db.models.tag }
+            ]
+          })
+      },
+
+      async createScreencast (screencast): any {
+        await db.sequelize.transaction(async transaction => {
+          const createdChannel = await db.models.channel.create({
+            id: screencast.channel.id,
+            name: screencast.channel.title
+          })
+          await createdChannel.createScreencast({
+            ...screencast
+          }, {
+            transaction
+          })
+          await db.models.tag.bulkCreate(screencast.tags, {
+            transaction
+          })
+          const screencastTags = screencast.tags.map(tag => {
+            return {
+              screencastId: screencast.id,
+              tagId: tag.id
+            }
+          })
+          await db.models.screencastTag.bulkCreate(screencastTags, {
+            transaction
+          })
         })
       }
     }
