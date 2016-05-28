8d74c17c6f3dba114724714a293c287b0923561f
Alex Booker <alexbooker@fastmail.im>
2016-05-28 21:46:04 +0100

    Refactor youtubeClientTest.js

--- test/integration/util/youtubeTest.js
+++ test/integration/util/youtubeTest.js
@@ -10,22 +10,6 @@ chai.use(chaiAsPromised)
 
 describe('youtube', () => {
   describe('fetchVideoDetails', () => {
-    it('should return correct video details', async () => {
-      const client = youtubeClient.create(config.youtubeApiKey)
-      const actual = await client.fetchVideoDetails('https://youtu.be/jNQXAC9IVRw')
-      expect(actual).to.exist
-      expect(actual).to.deep.equal({
-        id: 'jNQXAC9IVRw',
-        title: 'Me at the zoo',
-        description: 'The first video on YouTube.\n\nMaybe it\'s time to go back to the zoo?',
-        durationInSeconds: 19,
-        channel: {
-          id: 'UC4QobU6STFB0P71PMvOGN5A',
-          title: 'jawed'
-        }
-      })
-    })
-
     it('should throw when key is invalid', () => {
       const client = youtubeClient.create('invalid key')
       return expect(client.fetchVideoDetails('https://youtu.be/jNQXAC9IVRw'))
@@ -37,7 +21,7 @@ describe('youtube', () => {
         })
     })
 
-    it('should throw when video url is malformed', async () => {
+    it('should return null when video url is malformed', async () => {
       const client = youtubeClient.create(config.youtubeApiKey)
       const actual = await client.fetchVideoDetails('invalid')
       expect(actual).to.be.null
@@ -48,19 +32,34 @@ describe('youtube', () => {
       const actual = await client.fetchVideoDetails('https://youtu.be/dNAXAC8KVRw')
       expect(actual).to.be.null
     })
+
+    it('should return correct video details', async () => {
+      const client = youtubeClient.create(config.youtubeApiKey)
+      const actual = await client.fetchVideoDetails('https://youtu.be/jNQXAC9IVRw')
+      expect(actual).to.deep.equal({
+        id: 'jNQXAC9IVRw',
+        title: 'Me at the zoo',
+        description: 'The first video on YouTube.\n\nMaybe it\'s time to go back to the zoo?',
+        durationInSeconds: 19,
+        channel: {
+          id: 'UC4QobU6STFB0P71PMvOGN5A',
+          title: 'jawed'
+        }
+      })
+    })
   })
 
   describe('videoExists', () => {
     it('should return true when video exists', async () => {
       const client = youtubeClient.create(config.youtubeApiKey)
-      const exists = await client.videoExists('https://youtu.be/jNQXAC9IVRw')
-      expect(exists).to.be.true
+      const actual = await client.videoExists('https://youtu.be/jNQXAC9IVRw')
+      expect(actual).to.be.true
     })
 
     it('should return false when video doesn\'t exist', async () => {
       const client = youtubeClient.create(config.youtubeApiKey)
-      const exists = await client.videoExists('https://youtu.be/dNAXAC8KVRw')
-      expect(exists).to.be.false
+      const actual = await client.videoExists('https://youtu.be/dNAXAC8KVRw')
+      expect(actual).to.be.false
     })
   })
 })
